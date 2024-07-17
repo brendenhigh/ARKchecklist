dd17f2d8b576c115bd59d4c8c4c8659e3fed1e67
brendenhigh <117188061+brendenhigh@users.noreply.github.com>
2024-07-17 11:38:14 -0400

    Create welcome.php
    Added a description and created the main page.

---
+++ welcome.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset ="utf-8">
+		<title>Welcome Page</title>
+	</head?
+
+	<body>
+		<h1>Ark Checklist</h1>
+		<p>Hello and welcome to your checklist for Ark Survival Evolved.</p>
+		<p>If you are a new user, use the signup page to add yourself to the community.</p>
+		<p>For existing users, use the login tab to login to your checklist.</p>
+	</body>
+
+</html>
